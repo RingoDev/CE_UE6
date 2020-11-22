188414f3af5433478ce920872028c23eb8e2c508
ringodev <ringodev@protonmail.com>
2020-11-22 21:56:43 +0100

    added /options ednpoint

--- factory/src/main/java/com/ringodev/factory/Controller.java
+++ factory/src/main/java/com/ringodev/factory/Controller.java
@@ -1,2 +1,21 @@
-package com.ringodev.factory;public class Controller {
+package com.ringodev.factory;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api")
+public class Controller {
+
+
+    @PostMapping("/options")
+    public ResponseEntity<Object> getOptions(@RequestBody Configuration currentConfig){
+
+        return new ResponseEntity<>(new Configuration(),HttpStatus.OK);
+    }
+
+    static class Configuration{
+
+    }
 }
